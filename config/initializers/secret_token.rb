5d792a238ac0868d2d4f871afee1bb556e93889b
rgpass <rgpass@gmail.com>
2014-06-30 09:00:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happs::Application.config.secret_key_base = '36f0e479a5566ea013ea43a516ccab26b28031cc77e435c7a1f3fce4d893991fdcdc3d0b6b679285ddf70afb8054996fac5ddad80f092ab135d34ac643b6c1fe'
